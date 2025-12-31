54a346a016f210ccdaaebde0d8a241db819d297e
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2026-01-01 00:01:17 +0530

    Update inline.py

--- RaiChu/inline.py
+++ RaiChu/inline.py
@@ -64,7 +64,7 @@ def menu_markup(user_id):
       InlineKeyboardButton(text="▢", callback_data=f'cbstop | {user_id}')
     ],
      [InlineKeyboardButton(text="🔇", callback_data=f'cbmute | {user_id}'),
-      InlineKeyboardButton(text="ᴜᴩᴅᴀᴛᴇs", url=f"https://t.me/BotDuniyaXd"),
+      InlineKeyboardButton(text="ᴜᴩᴅᴀᴛᴇs", url=f"https://t.me/karunada_kings_kingdom"),
       InlineKeyboardButton(text="🔊", callback_data=f'cbunmute | {user_id}')],
   ]
   return buttons
